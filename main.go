741e7a6d3c90a54ba49d52e488f7a7a5e8cac2bc
John Eisberg <johneisberg@johns-mbp.attlocal.net>
2020-03-08 14:14:59 -0700

    fix(Users) add the users server

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	pb "github.com/jeisberg/go_protobuf_users/users"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedUserServiceServer
+}
+
+// SayHello implements helloworld.GreeterServer
+func (s *server) GetUser(ctx context.Context, in *pb.UserMessage) (*pb.UserMessage, error) {
+	
+	log.Printf("Received: %v", in.GetId())
+	
+	return &pb.UserMessage {
+		Id: "Id: " + in.GetId(),
+		Name: "Name: Captain Funk",
+		Email: "captainfunk@legit.com"
+	}, 
+	nil
+}
+
+func main() {
+	
+	lis, err := net.Listen("tcp", port)
+	
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	
+	s := grpc.NewServer()
+	
+	pb.RegisterUserServiceServer(s, &server{})
+	
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
